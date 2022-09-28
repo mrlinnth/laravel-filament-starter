6b4c7a402dcef77aa24387f864d7f87ac273d771
yan <yanlinnthor@evolxit.com>
2022-09-28 09:51:58 +0630

    Update user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,16 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\App;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens;
     use HasFactory;
@@ -45,4 +48,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function canAccessFilament(): bool
+    {
+        if (App::environment('local')) {
+            return true;
+        }
+
+        return str_ends_with($this->email, '@mail.com') && $this->hasVerifiedEmail();
+    }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
         extend: {
             colors: {
                 danger: colors.red,
-                primary: colors.pink,
+                primary: colors.blue,
                 success: colors.green,
                 warning: colors.yellow,
             },
