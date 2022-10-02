f4dc19a5f6a8a07809dd15a7014dd556d39d23a3
yan <yanlinnthor@evolxit.com>
2022-10-02 12:43:14 +0630

    Add user resource

--- app/Filament/Pages/Playground.php
+++ app/Filament/Pages/Playground.php
@@ -2,10 +2,13 @@
 
 namespace App\Filament\Pages;
 
+use BezhanSalleh\FilamentShield\Traits\HasPageShield;
 use Filament\Pages\Page;
 
 class Playground extends Page
 {
+    use HasPageShield;
+
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'filament.pages.playground';
