39460ae4b8d71d89715e4ffb5ab534ceb4184d55
yan <yanlinnthor@evolxit.com>
2022-10-26 11:08:40 +0630

    Update with avatar

--- resources/js/components/template/FullWidth.tsx
+++ resources/js/components/template/FullWidth.tsx
@@ -13,6 +13,11 @@ function FullWidth({ children }: any) {
                     </div>
 
                     <div className="mt-8 text-center">
+                        <img
+                            src="https://via.placeholder.com/150"
+                            alt=""
+                            className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
+                        />
                         <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">Cynthia J. Watts</h5>
                         <span className="hidden text-gray-400 lg:block">Admin</span>
                     </div>
